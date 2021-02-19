bef858696d92b2e63d6088a312f00504de229eca
Vincenzo Abete <abetevincenzo@gmail.com>
2021-02-19 18:14:21 +0000

    Testing with sending messages over UDP as well. Kart is too big

--- src/go_kart_go/NetworkCommunicationManager.java
+++ src/go_kart_go/NetworkCommunicationManager.java
@@ -41,36 +41,15 @@ public class NetworkCommunicationManager {
 
         if (!serverResponse.isBlank()) {
             if (serverResponse.equals(Messages.connectionSuccessful)) {
-                // if answer == connection successful then proceed
                 return true;
             }
-        } else {
-            // something went wrong. Can't talk to server
         }
+        // something went wrong. Can't talk to server
         return false;
     }
 
-    public int determinePlayer() {
-        // the idea is that the 1st person to connect to the server gets to be Player 1
-
-        String serverResponse = null;
-
-        // TCP
-        tcpClientCommunicationSocket.sendMessage(Messages.getPlayerNumber);
-        serverResponse = tcpClientCommunicationSocket.getMessage();
-
-        if (!serverResponse.isBlank()) {
-            return Integer.parseInt(serverResponse);
-        } else {
-            // something went wrong. Can't talk to server
-        }
-        return 0;
-    }
-
     public void sendKartInfo(Kart kart) {
-
         String serverResponse = null;
-        // TCP
         tcpClientCommunicationSocket.sendMessage(Messages.sendingKartInfo);
         tcpClientCommunicationSocket.sendKart(kart);
         serverResponse = tcpClientCommunicationSocket.getMessage();
@@ -83,27 +62,21 @@ public class NetworkCommunicationManager {
         } else {
             // something went wrong. Can't talk to server
         }
-
-
-//        // String message = "player:1;speed:10;index:10";
-//        String message = Messages.kartInfo(kart.getPlayer(), kart.getSpeed(), kart.getImageIndex());
-//        // send this player's kart info to server
-//        PacketSender.sendPacket(message, ServerDetails.getAddress(), ServerDetails.port, udpCommunicationSocket.socket);
     }
 
     public int getOpponentSpeed(Kart kart) {
         String message = Messages.getOpponentSpeed(kart.getPlayer());
         PacketSender.sendPacket(message, ServerDetails.getAddress(), ServerDetails.port, udpCommunicationSocket.socket);
-
-        String opponentSpeed = PacketReceiver.receivePacket(udpCommunicationSocket.socket);
-
-        if (!opponentSpeed.isBlank()) {
-            // ideally the answer is something like: player_X_speed:XX
-            // and we parse it here?
-            return Integer.parseInt(opponentSpeed);
-        } else {
-            // something went wrong. Can't talk to server
-        }
+//
+//        String opponentSpeed = packetReceiver.receivePacket(udpCommunicationSocket.socket);
+//
+//        if (!opponentSpeed.isBlank()) {
+//            // ideally the answer is something like: player_X_speed:XX
+//            // and we parse it here?
+//            return Integer.parseInt(opponentSpeed);
+//        } else {
+//            // something went wrong. Can't talk to server
+//        }
         return 0;
     }
 
@@ -111,15 +84,15 @@ public class NetworkCommunicationManager {
         String message = Messages.getOpponentIndex(kart.getPlayer());
         PacketSender.sendPacket(message, ServerDetails.getAddress(), ServerDetails.port, udpCommunicationSocket.socket);
 
-        String opponentImageIndex = PacketReceiver.receivePacket(udpCommunicationSocket.socket);
-
-        if (!opponentImageIndex.isBlank()) {
-            // ideally the answer is something like: player_X_index:XX
-            // and we parse it here?
-            return Integer.parseInt(opponentImageIndex);
-        } else {
-            // something went wrong. Can't talk to server
-        }
+//        String opponentImageIndex = packetReceiver.receivePacket(udpCommunicationSocket.socket);
+//
+//        if (!opponentImageIndex.isBlank()) {
+//            // ideally the answer is something like: player_X_index:XX
+//            // and we parse it here?
+//            return Integer.parseInt(opponentImageIndex);
+//        } else {
+//            // something went wrong. Can't talk to server
+//        }
         return 0;
     }
 
